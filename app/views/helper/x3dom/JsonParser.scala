7c0f6f29077f33d54c70ec3d625f3db7a5592e61
Markus Funke <funkemarkus@me.com>
2016-10-30 10:58:32 +0100

    small JsonParser improvements

--- app/views/helper/x3dom/JsonParser.scala
+++ app/views/helper/x3dom/JsonParser.scala
@@ -5,14 +5,16 @@ import play.api.libs.json._
 
 
 object JsonParser {
+  private val JSON_PATH = "app/assets/x3dom/x3dDefinition.json"
+  private val NODE_MODEL_BASE = "MODEL_BASE"
+  private val NODE_MODEL_CONN = "MODEL_CONNECTOR"
 
-  // INIT
   implicit private val quaternionFormat = Json.format[Quaternion]
   implicit private val vectorFormat = Json.format[Vector]
   implicit private val x3dModelBaseFormat = Json.format[x3dModelBase]
   implicit private val x3dModelConnectorFormat = Json.format[x3dModelConnector]
 
-  private val stream = new FileInputStream("app/assets/x3dom/x3dDefinition.json")
+  private val stream = new FileInputStream(JSON_PATH)
 
   private val json = try {
     Json.parse(stream)
@@ -20,9 +22,9 @@ object JsonParser {
     stream.close()
   }
 
-  private val modelBase = (json \ "MODEL_BASE").get
-  private val modelConn = (json \ "MODEL_CONNECTOR").get
+  private val modelBase = (json \ NODE_MODEL_BASE).get
+  private val modelConn = (json \ NODE_MODEL_CONN).get
 
-  val modelBaseList = Json.fromJson[List[x3dModelBase]](modelBase).get
-  val modelConnList = Json.fromJson[List[x3dModelConnector]](modelConn).get
+  var modelBaseList = Json.fromJson[List[x3dModelBase]](modelBase).get
+  var modelConnList = Json.fromJson[List[x3dModelConnector]](modelConn).get
 }
