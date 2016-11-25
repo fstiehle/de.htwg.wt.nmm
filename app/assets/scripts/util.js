e553ee7607106f99ce0880df564f76dca8307584
Markus Funke <mafunke@htwg-konstanz.de>
2016-11-25 17:09:54 +0100

    connector resizing work in progress

--- app/assets/scripts/util.js
+++ app/assets/scripts/util.js
@@ -3,4 +3,11 @@ $(window).resize(function() {
         Shapes.Junction[key].tune({ radius: document.getElementById("board").offsetWidth / 30 })
         .replay();
     }
+
+    Shapes.Connector.forEach(function(connector) {
+        connector.tune({ radius: connector.defaultLength * (connector.defaultLength / document.getElementById("board").offsetWidth) * 30 })
+        .replay();
+
+        console.log(connector.defaultLength * (connector.defaultLength / document.getElementById("board").offsetWidth) * 30);
+    });
 });
