b9861a734c575379a2b475be3611d17b2162d256
Fabian Stiehle <f.stiehle@gmail.com>
2016-11-26 14:53:23 +0100

    Comment the junction and connector scaling process

--- app/assets/scripts/util.js
+++ app/assets/scripts/util.js
@@ -1,19 +1,21 @@
-$(window).resize(function() {
 
-    var boardWidth = document.getElementById("board").offsetWidth;
+(function() {
+    $(window).resize(function() {
+        var boardWidth = document.getElementById("board").offsetWidth;
 
-    for (var key in Shapes.Junction) {
-        Shapes.Junction[key].tune({ radius: boardWidth / 30 })
-        .replay();
-    }
+        for (var key in Shapes.Junction) {
+            Shapes.Junction[key].tune({ radius: boardWidth / 30 })
+            .replay();
+        }
 
-    Shapes.Connector.forEach(function(connector) {
-        connector.tune({
-            radius: connector.defaultLength * boardWidth / 6,
-            radiusY: document.getElementById("board").offsetWidth / 90
-        })
-        .replay();
-
-        console.log(connector.defaultLength * boardWidth / 6);
+        Shapes.Connector.forEach(function(connector) {
+            connector.tune({
+                radius: connector.defaultLength * boardWidth / 6,
+                radiusY: document.getElementById("board").offsetWidth / 90
+            })
+            .replay();
+        });
     });
-});
+
+
+})();
