01942725402ff50dce213294a1d3c00dd1ce8c6c
Fabian Stiehle <f.stiehle@gmail.com>
2016-12-16 01:07:55 +0100

    Update and fix build proccess. Remove "typings" (now handeld through node/types"). Add global Less (with bootstrap).

--- app/angular2/gulpfile.js
+++ app/angular2/gulpfile.js
@@ -2,18 +2,20 @@
 
 const gulp = require("gulp");
 const del = require("del");
-const tsc = require("gulp-typescript");
+const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
-const tsProject = tsc.createProject("tsconfig.json");
+const tsProject = ts.createProject("tsconfig.json");
 const tslint = require('gulp-tslint');
 const less = require('gulp-less');
 const path = require('path');
+
+const DIST_PATH = "dist";
  
 /**
  * Remove build directory.
  */
 gulp.task('clean', (cb) => {
-    return del(["build"], cb);
+    return del([DIST_PATH], cb);
 });
 
 /**
@@ -30,27 +32,27 @@ gulp.task('tslint', () => {
 /**
  * Compile TypeScript sources and create sourcemaps in build directory.
  */
-gulp.task("compile", ["tslint", "less"], () => {
-    let tsResult = gulp.src("src/**/*.ts")
+gulp.task("ts", ["tslint"], () => {
+    let tsResult = tsProject.src() // load files based on tsconfig
         .pipe(sourcemaps.init())
-        .pipe(tsc(tsProject));
+        .pipe(tsProject());
     return tsResult.js
         .pipe(sourcemaps.write(".", {sourceRoot: '/src'}))
-        .pipe(gulp.dest("build"));
+        .pipe(gulp.dest(DIST_PATH));
 });
 
 /**
  * Compile Less to CSS
  */
- gulp.task('less', function () {
+ gulp.task('less', () => {
      return gulp.src("src/**/*.less")
-            .pipe(sourcemaps.init())
-            .pipe(less({paths: [
-                '.',
-                './node_modules/bootstrap-less'
-             ]}))
-            .pipe(sourcemaps.write(".", {sourceRoot: '/src'}))
-            .pipe(gulp.dest("build"));
+        .pipe(sourcemaps.init())
+        .pipe(less({paths: [
+            '.',
+            './node_modules/bootstrap-less'
+         ]}))
+        .pipe(sourcemaps.write(".", {sourceRoot: '/src'}))
+        .pipe(gulp.dest(DIST_PATH));
 });
 
 
@@ -59,7 +61,7 @@ gulp.task("compile", ["tslint", "less"], () => {
  */
 gulp.task("resources", () => {
     return gulp.src(["src/**/*", "!**/*.ts", "!**/*.less"])
-        .pipe(gulp.dest("build"));
+        .pipe(gulp.dest(DIST_PATH));
 });
 
 /**
@@ -75,16 +77,21 @@ gulp.task("libs", () => {
             'zone.js/dist/**',
             '@angular/**/bundles/**'
         ], {cwd: "node_modules/**"}) /* Glob required here. */
-        .pipe(gulp.dest("build/lib"));
+        .pipe(gulp.dest(DIST_PATH + "/lib"));
 });
 
 /**
  * Watch for changes in TypeScript, HTML and CSS files.
  */
-gulp.task('watch', function () {
-    gulp.watch(["src/**/*.ts", "src/**/*.less"], ['compile']).on('change', function (e) {
+gulp.task('watch', () => {
+    gulp.watch(["src/**/*.ts"], ['ts']).on('change', function (e) {
         console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
     });
+    
+    gulp.watch(["src/**/*.less"], ['less']).on('change', function (e) {
+        console.log('Less file ' + e.path + ' has been changed. Compiling.');
+    });
+    
     gulp.watch(["src/**/*.html"], ['resources']).on('change', function (e) {
         console.log('Resource file ' + e.path + ' has been changed. Updating.');
     });
@@ -93,6 +100,6 @@ gulp.task('watch', function () {
 /**
  * Build the project.
  */
-gulp.task("build", ['compile', 'resources', 'libs'], () => {
+gulp.task("build", ['ts', 'less', 'resources', 'libs'], () => {
     console.log("Building the project ...");
 });
