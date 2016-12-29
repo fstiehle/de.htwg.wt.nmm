b9ee4a20c38083df32002c480a87e299fcdba1a7
Fabian Stiehle <f.stiehle@gmail.com>
2016-12-29 15:06:30 +0100

    implement player form validation

--- app/angular2/src/app/app.module.ts
+++ app/angular2/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { RouterModule } from '@angular/router';
@@ -28,6 +28,7 @@ import { StatusComponent } from './play/status/status.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {
         path: 'play',
--- app/angular2/src/app/play/status/status.component.ts
+++ app/angular2/src/app/play/status/status.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-status',
@@ -10,13 +11,22 @@ export class StatusComponent implements OnInit {
   @Input() whiteName: String;
   @Input() blackName: String;
 
-  constructor() { }
+  playerForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.playerForm = fb.group({
+      'name' : [null, Validators.compose([Validators.required, Validators.minLength(2)])]
+    });
+  }
 
   ngOnInit() {
   }
 
+
   onSubmit() {
-    console.log("Submit!");
+    if (this.playerForm.valid) {
+      console.log("submit");
+    }
   }
 
 }
