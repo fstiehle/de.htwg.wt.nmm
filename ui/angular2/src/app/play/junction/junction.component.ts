8e91b46cd9185b0c0487aa533c413accecbe6ef9
Fabian Stiehle <f.stiehle@gmail.com>
2017-01-02 17:28:45 +0100

    cleanup imports

--- ui/angular2/src/app/play/junction/junction.component.ts
+++ ui/angular2/src/app/play/junction/junction.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { PlayService } from "../play.service";
 import Junction from './Junction';
 import Puck from '../puck/Puck';
