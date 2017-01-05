01e2c3de83c8a612aedee06cb5e15b7da3a39129
Fabian Stiehle <f.stiehle@gmail.com>
2017-01-05 19:13:47 +0100

    add color badge to status message depending on current player

--- ui/angular2/src/app/play/instructions/instructions.component.ts
+++ ui/angular2/src/app/play/instructions/instructions.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class InstructionsComponent implements OnInit {
 
   @Input() message: string;
+  @Input() currentPlayer: string;
 
   constructor() { }
 
